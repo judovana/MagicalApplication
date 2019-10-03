dc4b07a8967a084fd04ef226ebf424061b3109f4
Jiri Vanek <jvanek@redhat.com>
2019-10-03 14:13:14 +0200

    Refatored main to be more testable

--- src/main/java/org/judovana/test/App.java
+++ src/main/java/org/judovana/test/App.java
@@ -2,18 +2,27 @@ package org.judovana.test;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        output(create(readArgs(args)));
+    }
+
+    static int readArgs(String[] args) {
         int i = 5;
         if (args.length > 0) {
             i = Integer.valueOf(args[0]);
         }
-        Magican m = new Magican(i);
+        return i;
+    }
+
+    private static void output(Magican m) {
         m.printSetup(System.out);
         m.printMagic(System.out);
+    }
+
+    public static Magican create(int i) {
+        Magican m = new Magican(i);
+        return m;
     }
 }
